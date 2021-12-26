1f0c71bd46e79e0778015e5d8b5e4a85fbf21d3a
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-12-26 07:25:53 +0200

    task 04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import FriendList from "./FriendList/FriendList";
-import friends from "./FriendList/friends";
+import styled from 'styled-components';
+import FriendList from './components/FriendList/FriendList';
+import Profile from './components/Profile/Profile';
+import StatisticsTitle from './components/StatisticsTitle/StatisticsTitle';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import friends from './components/FriendList/friends';
+import user from './components/Profile/user.json';
+import data from './components/Statistics/data.json';
+import transactions from './components/TransactionHistory/transactions.json';
 
-function App() {
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 15px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
+    <AppWrapper>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <StatisticsTitle title={data.title} stats={data} />
       <FriendList friends={friends} />
-    </div>
+      <TransactionHistory items={transactions} />
+    </AppWrapper>
   );
-}
+};
 
 export default App;
